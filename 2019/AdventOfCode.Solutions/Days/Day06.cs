eed3fbbdeba0d3a206e8bb22e423191fc0b32a99
Pontus Magnusson <pontuspdmagnusson@gmail.com>
2020-12-04 00:15:50 +0100

    Create Day06.cs

---
+++ 2019/AdventOfCode.Solutions/Days/Day06.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Solutions.Days
+{
+    class Day06 : Day
+    {
+        public override void Part1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Part2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
