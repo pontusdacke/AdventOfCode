e15f1ddcc5499c1998d28bc3f64f1d80ca2a8310
Pontus Magnusson <pontuspdmagnusson@gmail.com>
2020-12-08 00:56:59 +0100

    Refactor day 7 part 1

--- 2020/src/AdventOfCode.Solutions/Days/Day07.cs
+++ 2020/src/AdventOfCode.Solutions/Days/Day07.cs
@@ -12,24 +12,23 @@ namespace PontusDacke.AdventOfCode2020.Solutions.Days
         {
             var timer = Stopwatch.StartNew();
             var bags = new HashSet<string>();
-            var previous = -1;
-
-            while (previous != bags.Count)
+            var previous = 0;
+            do
             {
                 previous = bags.Count;
                 foreach (var ruleInput in Inputs)
                 {
                     var rule = ruleInput.Split("contain");
-                    var bag = rule[0].Replace(" bags", string.Empty);
-                    var contains = rule[1];
+                    var bagColor = rule[0].Replace(" bags", string.Empty);
+                    var canHaveColors = rule[1];
 
-                    if (contains.Contains("shiny gold")
-                        || bags.Any(b => contains.Contains(b)))
+                    if (canHaveColors.Contains("shiny gold")
+                        || bags.Any(b => canHaveColors.Contains(b)))
                     {
-                        bags.Add(bag);
+                        bags.Add(bagColor);
                     }
                 }
-            }
+            } while (previous != bags.Count);
 
             timer.Stop();
             Console.WriteLine($"Part 1: {bags.Count}, in {timer.ElapsedMilliseconds} ms");
