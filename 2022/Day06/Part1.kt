9581e581f859d0724e0f8899e18651d4cd3dc093
Pontus Magnusson <pontuspdmagnusson@gmail.com>
2022-12-15 10:16:56 +0100

    Solve 2022 Day 6

---
+++ 2022/Day06/Part1.kt
@@ -0,0 +1,27 @@
+import java.io.File
+import java.io.InputStream
+import kotlin.collections.ArrayDeque
+
+fun main() {
+    val inputStream: InputStream = File("input.txt").inputStream()
+    val input = inputStream.bufferedReader().use { it.readText() }
+
+    val queue = ArrayDeque<Char>()
+
+    for ((i, char) in input.withIndex()) {
+        if (queue.size < 4) {
+            queue.addLast(char)
+            continue
+        }
+
+        if (queue.distinct().size == queue.size) {
+            print(i)
+            break
+        }
+
+        queue.removeFirst()
+        queue.addLast(char)
+    }
+}
+
+//kotlinc Part1.kt -include-runtime -d test.jar && kotlin -classpath test.jar Part1Kt
