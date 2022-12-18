eb54f3c5fe3af4bb442e5ee6f33ad91f000f3ee0
Pontus Magnusson <pontus.magnusson@soderbergpartners.se>
2022-12-18 21:57:02 +0100

    Add 2022 Day 11 template

---
+++ 2022/Day11/Part1.kt
@@ -0,0 +1,14 @@
+import java.io.File
+import java.io.InputStream
+
+fun main() {
+    val inputStream: InputStream = File("input.txt").inputStream()
+    val input = inputStream.bufferedReader().use { it.readText() }
+}
+
+/*
+Mac
+kotlinc Part1.kt -include-runtime -d test.jar && kotlin -classpath test.jar Part1Kt
+Windows
+kotlinc Part1.kt -include-runtime -d test.jar; kotlin -classpath test.jar Part1Kt
+*/
